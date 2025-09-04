e34924af8fae90c7c33c5c762fe5475d4373dc71
Ashik Nizam <99461353+ashiknizamdeen@users.noreply.github.com>
2025-09-05 00:52:35 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -62,7 +62,7 @@ const Hero = () => {
           <motion.div className="hero-image-container" variants={imageVariants}>
             <div className="profile-image-wrapper">
               <img 
-                src="/proPic.jpg"
+                src="/proPic.png"
                 alt="Ashik Nizamdeen"
                 className="profile-image"
               />
